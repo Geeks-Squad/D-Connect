75d52f2593d05633030436c9ff5cf31e35568114
Shubham Maheshwari <shub97@gmail.com>
2017-03-31 02:49:42 +0530

    Added ASyncTasks

--- app/src/main/java/com/india/gov/helperapp/LoginActivity.java
+++ app/src/main/java/com/india/gov/helperapp/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.india.gov.helperapp;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
@@ -12,15 +11,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.IOException;
+import com.india.gov.helperapp.service.LoginTask;
 
 import butterknife.ButterKnife;
 import butterknife.Bind;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 /**
  * Created by vyas on 3/25/17.
@@ -71,76 +65,22 @@ public class LoginActivity extends AppCompatActivity {
 
         _loginButton.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
+        String username = _usernameText.getText().toString();
+        String password = _passwordText.getText().toString();
 
+        LoginTask loginTask = new LoginTask(username,password,getApplicationContext(),this);
+        loginTask.execute();
+        _loginButton.setEnabled(true);
 
-        // TODO: Implement your own authentication logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        String username = _usernameText.getText().toString();
-                        String password = _passwordText.getText().toString();
-
-                        String result;
-                        MediaType JSON
-                                = MediaType.parse("application/json; charset=utf-8");
-
-                        OkHttpClient client = new OkHttpClient();
-                        String json = "{username:\""+username+"\",password:\""+password+"\"}";
-                        String url = "192.168.43.143:8080";
-                        RequestBody body = RequestBody.create(JSON, json);
-                        Request request = new Request.Builder()
-                                .url(url)
-                                .post(body)
-                                .build();
-                        Response response = null;
-                        try {
-                            response = client.newCall(request).execute();
-                            result = response.body().string();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            return;
-                        }
-
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_SIGNUP) {
-            if (resultCode == RESULT_OK) {
-
-                // TODO: Implement successful signup logic here
-                // By default we just finish the Activity and log them in automatically
-                this.finish();
-            }
-        }
     }
 
     @Override
     public void onBackPressed() {
-        // Disable going back to the MainActivity
         moveTaskToBack(true);
-    }
-
-    public void onLoginSuccess() {
-        _loginButton.setEnabled(true);
-        finish();
     }
 
     public void onLoginFailed() {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-        _loginButton.setEnabled(true);
     }
 
     public boolean validate() {
---
+++ app/src/main/java/com/india/gov/helperapp/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.india.gov.helperapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                        startActivity(i);
+
+                        // close this activity
+                        finish();
+                    }
+                }, SPLASH_TIME_OUT);
+    }
+}
